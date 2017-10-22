603be5c2940e2e58d406c9b456ab7d9a09626d05
VictorMunozP <victor.munoz.pesce@gmail.com>
2017-10-22 05:02:02 -0300

    home bueno con .vue

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -18,7 +18,8 @@ export default new Router({
 */
 import Vue from 'vue' // Import Vue from node_modules
 import Router from 'vue-router' // Import Vue Router from node_modules
-import HelloWorld from '@/components/HelloWorld' //The Home component that's in charge of everything we see on the app's homepage
+import Home from '@/components/Home' //The Home component that's in charge of everything we see on the app's homepage
+//import HelloWorld from '@/components/HelloWorld' //The Home component that's in charge of everything we see on the app's homepage
 import VueChartJS from '@/components/VueChartJS' //The VueChartJS component that displays the vue-chartjs charts.
 import VueChartKick from '@/components/VueChartKick' //The VueChartJS component that displays the vue-chartkick charts.
 import VueCharts from '@/components/VueCharts' //The VueChartJS component that displays the vue-charts charts.
@@ -28,10 +29,15 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },*/
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
     },
     {
       path: '/chartjs',
